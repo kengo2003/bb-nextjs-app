93ff62084116c3dd17e8697c5eeb9f37e30d29a9
kengo2003 <saitokengo2003@gmail.com>
2024-06-25 00:54:41 +0900

    編集と削除ボタン実装

--- app/posts/[Id]/page.tsx
+++ app/posts/[Id]/page.tsx
@@ -1,4 +1,5 @@
 import { BBDataType } from "@/app/types/types";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 
@@ -16,7 +17,7 @@ const DetailPage = async ({ params }: { params: { Id: number } }) => {
   const bbDetailData = await getDetailData(params.Id);
   const { title, content, username } = bbDetailData;
   return (
-    <div className="mx-auto max-w-4xl p-10 border border-gray-300 bg-white">
+    <div className="mx-auto max-w-4xl p-10 border border-gray-300 bg-white  text-white">
       <div className="mb-8">
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-gray-700">{username}</p>
@@ -25,13 +26,23 @@ const DetailPage = async ({ params }: { params: { Id: number } }) => {
       <div className="mb-8">
         <p className="text-gray-900">{content}</p>
       </div>
-
-      <Link
-        href={"/"}
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md"
-      >
-        戻る
-      </Link>
+      <div className="w-full flex">
+        <div className="w-full">
+          <Link href={"/"}>
+            <Button className="w-1/4 bg-blue-500">戻る</Button>
+          </Link>
+        </div>
+        <div className="w-full text-right">
+          <Link href={"/edit"}>
+            <Button className="w-1/4">編集</Button>
+          </Link>
+          <Link href={"/delete"}>
+            <Button className="ml-8 w-1/4 bg-red-600 border border-slate-500">
+              削除
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
