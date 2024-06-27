1f50ad619859a6438e78c9012cd748c1109ab7fe
kengo2003 <saitokengo2003@gmail.com>
2024-06-28 01:13:43 +0900

    post削除　実装

--- app/api/post/[Id]/route.ts
+++ app/api/post/[Id]/route.ts
@@ -1,4 +1,6 @@
 import prisma from "@/lib/prismaClient";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -11,5 +13,22 @@ export async function GET(
       id: parseInt(bbId),
     },
   }); //投稿が膨大になった時には改修必要
-  return NextResponse.json(bbDetailData);
+  return Response.json(bbDetailData);
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { Id: string } }
+) {
+  const bbId = params.Id;
+  try {
+    await prisma.post.delete({
+      where: {
+        id: parseInt(bbId),
+      },
+    });
+    console.log("log: delete OK");
+  } catch (error) {
+    console.error("log: delete error");
+  }
 }
