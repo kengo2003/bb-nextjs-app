cff4569876404bc96cba71dea14da353da8aee63
kengo2003 <saitokengo2003@gmail.com>
2024-07-10 23:03:25 +0900

    Edit Header

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # 掲示板アプリ
-## 使用技術：
-TyprScript
-Next.js
-Prisma
-Supabase
 
+## 実装機能
+
+・新規記事投稿  
+作成した記事は DB に保存されます  
+・記事削除  
+DB からも削除されます  
+・記事編集  
+投稿済みの記事を編集して再投稿できます
+
+## 使用技術：
+
+TyprScript  
+Next.js  
+Prisma  
+Supabase  
+(準備中)Vercel or cloudflare
--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,24 +1,18 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="divide-y border-gray-400 dark:border-gray-800 border-b bg-white">
-      <div className="px-4 py-3 md:py-6 lg:px-6">
-        <div className="flex items-center space-y-2 md:space-y-0 md:space-x-6">
+      <div className="px-4 py-3 md:py-6 lg:px-10">
+        <div className="grid grid-cols-2 items-center space-y-2 md:space-y-0 md:space-x-6">
           <Link
             href="/"
-            className="text-2xl font-bold tracking-tighter mr-4 text-black"
+            className="text-2xl font-bold tracking-tighter text-black"
           >
-            Bulletin Board
+            掲示板アプリ
           </Link>
-          <nav className="flex items-center space-x-6 text-sm">
-            <Link
-              className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-              href="/"
-            >
-              Home
-            </Link>
+          <nav className="flex items-center space-x-6 text-sm justify-self-end">
             <Link
               className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
               href="/about"
@@ -29,13 +23,13 @@ const Header = () => {
               className="bg-black py-3 px-4 text-white rounded-md font-medium"
               href="/posts/create"
             >
-              Create Post
+              新規投稿
             </Link>
           </nav>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
